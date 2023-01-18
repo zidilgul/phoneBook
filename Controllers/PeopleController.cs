0fcca6f4f437249c5531eb870f8597364290baf5
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-18 17:09:50 +0300

    edit contact added

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -16,7 +16,7 @@ namespace phoneBook.Controllers
         [HttpGet]
         public ActionResult ListPeople(int id)
         {
-            return View(db.peopleInDirectory.Where(x=> x.userId == id).ToList());
+            return View(db.peopleInDirectory.Where(x => x.userId == id).ToList());
         }
 
         public ActionResult Create()
@@ -27,24 +27,66 @@ namespace phoneBook.Controllers
         [HttpPost]
         public ActionResult Create(peopleInDirectory person)
         {
-            if (Session["idSS"] != null) {
+            if (Session["idSS"] != null)
+            {
                 person.userId = int.Parse(Session["idSS"].ToString());
 
-                if (db.peopleInDirectory.Any(x=> x.userId == person.userId && x.phone == person.phone))
-            {
-                ViewBag.Notification = "Bu telefon rehberinizde zaten kayıtlı!";
-                return View();
-            }
-            else
-            {
-                db.peopleInDirectory.Add(person);
-                db.SaveChanges();
+                if (db.peopleInDirectory.Any(x => x.userId == person.userId && x.phone == person.phone))
+                {
+                    ViewBag.Notification = "Bu telefon rehberinizde zaten kayıtlı!";
+                    return View();
+                }
+                else
+                {
+                    db.peopleInDirectory.Add(person);
+                    db.SaveChanges();
                     return RedirectToAction("ListPeople", "People", new { id = person.userId });
                 }
             }
             else
             {
                 ViewBag.Notification = "Lütfen giriş yapın!";
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var p = db.peopleInDirectory.Where(w => w.id == id).FirstOrDefault();
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Edit(peopleInDirectory person)
+        {
+            person.userId = int.Parse(Session["idSS"].ToString());
+            if (person.fullName != null && person.phone != null)
+            {
+                if (db.peopleInDirectory.Any(a => a.userId == person.userId && a.phone == person.phone))
+                {
+                    ViewBag.Notification = "Bu telefon rehberinizde zaten kayıtlı!";
+                    return View();
+                }
+
+                int i = int.Parse(Session["idSS"].ToString());
+                var p = db.peopleInDirectory.Where(x => x.id == person.id && x.userId == i).FirstOrDefault();
+
+                if (p == null)
+                {
+                    ViewBag.Notification = "Böyle bir kişi mevcut değil.";
+                    return View();
+                }
+                else
+                {
+                  
+                    p.phone = person.phone;
+                    p.fullName = person.fullName;
+                    db.SaveChanges();
+                    return RedirectToAction("ListPeople", "People", new { id = p.userId });
+                }
+            }
+            else
+            {
+                ViewBag.Notification = "Lütfen boş alanları doldurun!";
                 return View();
             }
         }
